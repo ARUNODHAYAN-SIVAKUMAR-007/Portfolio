0a0c04621452d50138c1bc4b7980151e9a17e6c9
ARUNODHAYAN-SIVAKUMAR-007 <arunodhayans15@gmail.com>
2025-09-17 22:04:57 +0800

    update

--- script.js
+++ script.js
@@ -13,16 +13,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
     });
-    navLinks.classList.remove('active');
+    navLinks.classList.remove('active'); // close menu on click
   });
 });
 
 // Keep clicked nav link active
 const links = document.querySelectorAll('.nav-links li a');
-
 links.forEach(link => {
   link.addEventListener('click', () => {
-    links.forEach(l => l.classList.remove('active')); // remove old
-    link.classList.add('active'); // highlight clicked
+    links.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
   });
 });
